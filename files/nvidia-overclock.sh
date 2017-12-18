d94b9ef67f75d0e9eb3ad638660265d202e251c6
1050ti <34488237+1050ti@users.noreply.github.com>
2017-12-18 19:42:51 +0100

    fixed indentation and added comment on perfomance state

--- files/nvidia-overclock.sh
+++ files/nvidia-overclock.sh
@@ -17,10 +17,10 @@ export DISPLAY=:0
 
 # returns the performance level of the specified device
 function getPerfLevel () {
-    local lineCount=0
-    local deviceNo=$1
-    nvidia-smi --format=csv,noheader --query-gpu=name | while read line
-    do
+	local lineCount=0
+	local deviceNo=$1
+	nvidia-smi --format=csv,noheader --query-gpu=name | while read line
+	do
 		if (( lineCount == deviceNo )); then
 			if [[ $line == *"1050"* ]]; then
 				return 2
@@ -38,8 +38,10 @@ for MY_DEVICE in {0..5}
 do
 	# Check if card exists
 	if nvidia-smi -i $MY_DEVICE >> /dev/null 2>&1; then
+		#detect the erformance level
 		getPerfLevel $MY_DEVICE
 		perfLevel=$?
+		# Put card into highest perfomance state
 		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
 		# Fan speed
 		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
@@ -48,8 +50,8 @@ do
 		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[$perfLevel]=$MY_CLOCK"
 		# Memory clock
 		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[$perfLevel]=$MY_MEM"
-        # Set watt/powerlimit. This is also set in miner.sh at autostart.
-        sudo nvidia-smi -i "$MY_DEVICE" -pl "$MY_WATT"
+		# Set watt/powerlimit. This is also set in miner.sh at autostart.
+		sudo nvidia-smi -i "$MY_DEVICE" -pl "$MY_WATT"
 	fi
 done
 
